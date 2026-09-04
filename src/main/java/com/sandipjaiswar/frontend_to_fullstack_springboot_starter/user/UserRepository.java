a6a4f59d71d85a2664c146f101a01bf710231ba9
Sandip Jaiswar <sandipjaiswar@gmail.com>
2026-09-05 01:36:52 +0530

    Create UserRepository.java

---
+++ src/main/java/com/sandipjaiswar/frontend_to_fullstack_springboot_starter/user/UserRepository.java
@@ -0,0 +1,12 @@
+package com.sandipjaiswar.frontend_to_fullstack_springboot_starter.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
