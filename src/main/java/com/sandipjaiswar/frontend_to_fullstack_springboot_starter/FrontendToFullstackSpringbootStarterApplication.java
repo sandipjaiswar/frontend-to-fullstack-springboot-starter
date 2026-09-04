72740f3a4d67a9d2716f0c392703d116d733fbb2
Sandip Jaiswar <sandipjaiswar@gmail.com>
2026-09-05 01:04:23 +0530

    Add files via upload
    Add Spring Boot starter project

---
+++ src/main/java/com/sandipjaiswar/frontend_to_fullstack_springboot_starter/FrontendToFullstackSpringbootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.sandipjaiswar.frontend_to_fullstack_springboot_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendToFullstackSpringbootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendToFullstackSpringbootStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandipjaiswar/frontend_to_fullstack_springboot_starter/FrontendToFullstackSpringbootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandipjaiswar.frontend_to_fullstack_springboot_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendToFullstackSpringbootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
